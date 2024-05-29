7ad95026f3012673145f5657bf58762b185f08e4
carovides <jaidercaro@gmail.com>
2024-05-28 19:47:25 -0500

    primercommit

---
+++ script.js
@@ -0,0 +1,47 @@
+/** Crear un algoritmo que reciba el alto y el ancho de un rectangulo y muestre en pantalla su area. */
+  // Ar = Alto*Ancho
+   //  var base = parseIntpr(prompt("ingrese la base del rectangulo: "))
+    // var altura = parseInt(prompt("ingrese la altura del rectangulo "))
+   //  var area = base*altura
+  //  document.write("El area del triangulo es:" + area)
+
+  var Radio = parseFloat(prompt("ingrese el radio del circulo"))/** Crear un algoritmo que reciba el alto y el ancho de un rectangulo y muestre en pantalla su area. */
+ 
+ // Ar = Alto*Ancho
+  //  var base = parseIntpr(prompt("ingrese la base del rectangulo: "))
+   // var altura = parseInt(prompt("ingrese la altura del rectangulo "))
+  //  var area = base*altura
+ //  document.write("El area del triangulo es:" + area)
+
+
+// Ar = Alto*Ancho
+ //  var base = parseIntpr(prompt("ingrese la base del rectangulo: "))
+  // var altura = parseInt(prompt("ingrese la altura del rectangulo "))
+ //  var area = base*altura
+//  document.write("El area del triangulo es:" + area)
+
+var Radio = parseFloat(prompt("ingrese el radio del circulo"))/** Crear un algoritmo que reciba el alto y el ancho de un rectangulo y muestre en pantalla su area. */
+// Ar = Alto*Ancho
+ //  var base = parseIntpr(prompt("ingrese la base del rectangulo: "))
+  // var altura = parseInt(prompt("ingrese la altura del rectangulo "))
+ //  var area = base*altura
+//  document.write("El area del triangulo es:" + area)
+
+var Radio = parseFloat(prompt("ingrese el radio del circulo"))
+/** Crear un algoritmo que reciba el alto y el ancho de un rectangulo y muestre en pantalla su area. */
+// Ar = Alto*Ancho
+ //  var base = parseIntpr(prompt("ingrese la base del rectangulo: "))
+  // var altura = parseInt(prompt("ingrese la altura del rectangulo "))
+ //  var area = base*altura
+//  document.write("El area del triangulo es:" + area)
+
+var Radio = parseFloat(prompt("ingrese el radio del circulo"))
+/** Crear un algoritmo que reciba el alto y el ancho de un rectangulo y muestre en pantalla su area. */
+// Ar = Alto*Ancho
+ //  var base = parseIntpr(prompt("ingrese la base del rectangulo: "))
+  // var altura = parseInt(prompt("ingrese la altura del rectangulo "))
+ //  var area = base*altura
+//  document.write("El area del triangulo es:" + area)
+
+var Radio = parseFloat(prompt("ingrese el radio del circulo"))
+<img src="https://3.bp.blogspot.com/-e4rYTSwn8Fg/UYGHHTzFX4I/AAAAAAABwKk/CoymthQWNKs/s1600/hermoso-atardecer-amazing-sunset.jpg"width="17%">
